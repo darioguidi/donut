3a722c235091cd0222ab17261d1f7ec451357be1
Dario Guidi <99537338+darioguidi@users.noreply.github.com>
2025-04-18 00:18:02 +0200

    .

--- donut.c
+++ donut.c
@@ -1,49 +1,60 @@
+#include <X11/Xlib.h>
 #include <stdio.h>
-#include <math.h>
-#include <stdint.h>
-#include <string.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <math.h>
 
-int main() {
-    float A = 0, B = 0;
-    float i, j;
-    int k;
-    float z[1760];
-    char b[1760];
-    printf("\x1b[2J");
-    for(;;) {
-        memset(b,32,1760);
-        memset(z,0,7040);
-        for(j=0; j < 6.28; j += 0.07) {
-            for(i=0; i < 6.28; i += 0.02) {
-                float c = sin(i);
-                float d = cos(j);
-                float e = sin(A);
-                float f = sin(j);
-                float g = cos(A);
-                float h = d + 2;
-                float D = 1 / (c * h * e + f * g + 5);
-                float l = cos(i);
-                float m = cos(B);
-                float n = sin(B);
-                float t = c * h * g - f * e;
-                int x = 40 + 30 * D * (l * h * m - t * n);
-                int y= 12 + 15 * D * (l * h * n + t * m);
-                int o = x + 80 * y;
-                int N = 8 * ((f * e - c * d * g) * m - c * d * e - f * g - l * d * n);
-                if(22 > y && y > 0 && x > 0 && 80 > x && D > z[o]) {
-                    z[o] = D;
-                    b[o] = ".,-~:;=!*#$@"[N > 0 ? N : 0];
-                }
-            }
-        }
-        printf("\x1b[H");
-        for(k = 0; k < 1761; k++) {
-            putchar(k % 80 ? b[k] : 10);
-            A += 0.00004;
-            B += 0.00002;
-        }
-        usleep(30000);
+#define M_PI 3.14159265358979323846
+
+void getCoordinates(double R1, double R2, double theta, double *x, double *y, double *z){
+    *x = R2 + R1 * cos(theta);
+    *y = R1 * sin(theta);
+    *z = 0.0;
+}
+
+int main(void){
+    
+    // Connessione con il display
+    Display *display = XOpenDisplay(NULL);
+    if (display == NULL) {
+        fprintf(stderr, "Impossibile aprire il display\n");
+        return 1;
     }
+
+    // Recupera lo schermo corrente
+    int screen = DefaultScreen(display);
+
+    // Crea la finestra
+    Window window = XCreateSimpleWindow(
+        display,
+        RootWindow(display, screen),
+        10, 10,                 // posizione
+        400, 400,               // larghezza, altezza
+        1,                      // bordo
+        BlackPixel(display, screen), // colore bordo
+        WhitePixel(display, screen)  // colore sfondo
+    );
+
+    // Mostra la finestra
+    XMapWindow(display, window);
+    XFlush(display);
+
+    // Esegui il calcolo delle coordinate
+    double R1 = 1.0, R2 = 2.0, theta = M_PI / 4;
+    double x, y, z;
+    getCoordinates(R1, R2, theta, &x, &y, &z);
+    printf("x = %f, y = %f, z = %f\n", x, y, z);
+
+    // Ciclo di eventi per mantenere la finestra aperta
+    XEvent event;
+    while (1) {
+        XNextEvent(display, &event);  // Attende un evento
+        if (event.type == DestroyNotify) {
+            break;  // Esce quando la finestra è chiusa
+        }
+    }
+
+    // Chiudi la connessione con il display
+    XCloseDisplay(display);
     return 0;
 }
